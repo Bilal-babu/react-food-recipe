b332493b73ba16138b0dd3cb67c41997216167d5
Bilal-babu <bz91896@gmail.com>
2020-11-26 11:58:24 +0500

    food recipe website

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h5>&copy; Copyright Reserved || Template By Babu</h5>
+    </div>
+  );
+}
+
+export default Footer;
